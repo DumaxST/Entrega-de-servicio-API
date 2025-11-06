65304bef8d4101909185fa4f6979102620b164aa
Erika Flores <eflores@dumaxst.com>
2025-11-05 21:41:36 -0600

    feat| create account  datasources and repositories

---
+++ functions/src/domain/repositories/account.repository.ts
@@ -0,0 +1,11 @@
+import { CreateAccountDTO } from "domain/dtos";
+import { AccountEntity } from "domain/entities/account.entity";
+
+export abstract class AccountRepository {
+   abstract createAccount(createAccountDto: CreateAccountDTO):Promise<AccountEntity>;
+   //TODO: agregar páginación y filtros
+   abstract getAll():Promise<AccountEntity[]>;
+
+   abstract getById(id:string):Promise<AccountEntity | null>
+   abstract updateAccount(id:string):Promise<AccountEntity | null>
+}
